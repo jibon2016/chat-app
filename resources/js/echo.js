f5c544386345f1c73136d086f180a55937669be1
Jibon Sutradhar <sdjibon2016@gmail.com>
2025-08-29 18:14:54 +0600

    Add Public/build folder

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -12,4 +12,12 @@ window.Echo = new Echo({
     wssPort: Number(import.meta.env.VITE_PUSHER_PORT || 443),
     forceTLS: import.meta.env.VITE_PUSHER_USETLS === 'true',
     enabledTransports: ['ws', 'wss'],
+
+
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
